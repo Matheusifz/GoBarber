c4c9ee88e5938b551f239d92660426f1e27d8923
matheusifz <matheusifz@gmail.com>
2021-11-16 17:50:53 -0300

    chore: create a script to start the server

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (request, response) => {
-  return response.json({ message: "Hello World " });
+  return response.json({ message: "Cheers!  " });
 });
 
 app.listen(3333, () => {
